1d04f6aacac80d8b91c0d183b78ded89a5dc523a
Riley Rockwell <rileyrockwell@gmail.com>
2024-11-15 08:59:21 -0800

    updates

--- edX/UNIT_2/finger_exercises/2.6_random_walks/ex3.py
+++ edX/UNIT_2/finger_exercises/2.6_random_walks/ex3.py
@@ -1,4 +1,30 @@
 import random
 
-a = random.randint(1, 10)
-print(a)
+random.seed(9001)
+for i in range(random.randint(1, 10)):
+	print(random.randint(1, 10))
+
+print("###")
+
+random.seed(9001)
+d = random.randint(1, 10)
+for i in range(random.randint(1, 10)):
+    print(d)
+
+print("###")
+
+random.seed(9001)
+d = random.randint(1, 10)
+for i in range(random.randint(1, 10)):
+    if random.randint(1, 10) < 7:
+        print(d)
+    else:
+        random.seed(9001)
+        d = random.randint(1, 10)
+        print(random.randint(1, 10))
+
+
+# supposedly:
+# 3
+# 1, 1, 1
+# 1, 1, 3
