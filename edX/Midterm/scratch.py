68318c79ba3db429fb730c182da7c333d81fc147
Riley Rockwell <rileyrockwell@gmail.com>
2024-11-25 21:29:25 +0000

    updates

--- edX/Midterm/scratch.py
+++ edX/Midterm/scratch.py
@@ -1,1 +1,14 @@
-pass
+def song_playlist(songs, max_size):
+    """
+    songs: list of tuples, ('song_name', song_len, song_size)
+    max_size: float, maximum size of total songs that you can fit
+
+    Start with the song first in the 'songs' list, then pick the next 
+    song to be the one with the lowest file size not already picked, repeat
+
+    Returns: a list of a subset of songs fitting in 'max_size' in the order 
+             in which they were chosen.
+    """
+    pass
+
+
