bdebc39fbd08d46e85488fbde7e5ec00abb71ca5
Riley Rockwell <rileyrockwell@gmail.com>
2024-11-25 05:41:23 +0000

    updates

--- edX/UNIT_2/lecture_slides_code/2.6_random_walks/2.6.2_simulating_a_walk/scrap.py
+++
@@ -1,4 +0,0 @@
-random.seed(0)
-simAll((UsualDrunk, ColdDrunk),
-       (1, 10, 100, 1000, 10000), 100)
-
--- edX/UNIT_2/lecture_slides_code/2.6_random_walks/2.6.3_random_walks/scratch.py
+++ edX/UNIT_2/lecture_slides_code/2.6_random_walks/2.6.3_random_walks/scratch.py
@@ -3,6 +3,3 @@ from lect6segment3 import *
 origin = Location(0, 0)
 point1 = Location(0, 1)
 point2 = Location(1, 0)
-
-
-
