eef41ded7a42e54eebd0c255945630a128bbd13a
Riley Rockwell <rileyrockwell@gmail.com>
2024-11-25 19:38:57 +0000

    updates

--- edX/Miscellaneous/simulation.py
+++ edX/Miscellaneous/simulation.py
@@ -1,7 +1,20 @@
 import random
 random.seed(9001)
 
-random.seed(9001)
 for i in range(random.randint(1, 10)):
     print(random.randint(1, 10))
 
+d = random.randint(1, 10)
+for i in range(random.randint(1, 10)):
+    print(d)
+
+print("###")
+
+d = random.randint(1, 10)
+for i in range(random.randint(1, 10)):
+    if random.randint(1, 10) < 7:
+        print(d)
+    else:
+        random.seed(9001)
+        d = random.randint(1, 10)
+        print(random.randint(1, 10))
