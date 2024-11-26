196a4be5e3f116ce798ac8eae2c7bd17a417524f
Riley Rockwell <rileyrockwell@gmail.com>
2024-11-26 01:56:49 +0000

    updates

--- edX/Midterm/scratch.py
+++ edX/Midterm/scratch.py
@@ -10,14 +10,26 @@ def song_playlist(songs, max_size):
              in which they were chosen.
     """
 
-    # return songs
+    # sort the songs details (tuple) according to the size of the individual songs
+    sorted_capacity = sorted(songs, key=lambda x: x[2], reverse = False)
 
-    sorted_capacity = sorted(songs, key=lambda x: x[2], reverse = True)
+    # initialize solution and total cost / value trackes
+    solution = []
+    total_cost = 0
+    total_value = 0
 
-    return sorted_capacity
+    # iterate over the sorted_capacity list and select locally opitmal choices
+    for element in sorted_capacity:
+        if total_cost + element[2] <= max_size:
+            solution.append(element[0])
+            total_cost += element[2]
+
+    # return the result
+    return solution
 
 
 
 songs_specs = songs = [('Roar',4.4, 4.0),('Sail',3.5, 7.7),('Timber', 5.1, 6.9),('Wannabe',2.7, 1.2)]
-max_size = 10
+max_size = 11
 print(song_playlist(songs_specs, max_size))
+
