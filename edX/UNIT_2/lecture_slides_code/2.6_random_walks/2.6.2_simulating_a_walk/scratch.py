2ce551ade3949cad0c8b7aeb165b0665b8158919
Riley Rockwell <rileyrockwell@gmail.com>
2024-12-03 21:39:55 +0000

    updates

--- edX/UNIT_2/lecture_slides_code/2.6_random_walks/2.6.2_simulating_a_walk/scratch.py
+++
@@ -1,8 +0,0 @@
-from lecture_code import *
-
-###
-
-instance = Location(0, 0)
-instance.move(1, 1)
-print(instance)
-print(instance.move(1, 1))
--- edX/UNIT_2/lecture_slides_code/2.6_random_walks/2.6.3_random_walks/scratch.py
+++
@@ -1,6 +0,0 @@
-from lect6segment3 import *
-
-origin = Location(0, 0)
-point1 = Location(0, 1)
-point2 = Location(1, 0)
-point3 = Location(1, 1)
