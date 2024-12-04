5ab8e44af156d04847c2719d45f89b34b92d4081
Riley Rockwell <rileyrockwell@gmail.com>
2024-12-04 01:49:37 +0000

    updates

--- edX/UNIT_3/3.8_monte_carlo_simulations/3.8.7_ex4/no_replacement_simulation.py
+++ edX/UNIT_3/3.8_monte_carlo_simulations/3.8.7_ex4/no_replacement_simulation.py
@@ -1,5 +1,12 @@
-def noRepalcementSimulation(numTrials):
-    return -1
+def noReplacementSimulation(numTrials):
+    '''
+    Runs numTrials trials of a Monte Carlo simulation
+    of drawing 3 balls out of a bucket containing
+    3 red and 3 green balls. Balls are not replaced once
+    drawn. Returns a decimal - the fraction of times 3 
+    balls of the same color were drawn.
+    '''
+    
 
 
 n = 1
